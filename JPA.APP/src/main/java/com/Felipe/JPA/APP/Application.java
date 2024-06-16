6f2a19f712d456c5ff8e5ca40c9989cd05fa98fd
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-15 19:22:01 -0500

    add Repository & method run
    como es un proyecto desarrollado en consola implementamos el metodo run y dentro de ello lo que queremos llamar en consola en este caso la lista de todas las personas.

--- JPA.APP/src/main/java/com/Felipe/JPA/APP/Application.java
+++ JPA.APP/src/main/java/com/Felipe/JPA/APP/Application.java
@@ -1,13 +1,27 @@
 package com.Felipe.JPA.APP;
 
+import com.Felipe.JPA.APP.entities.Person;
+import com.Felipe.JPA.APP.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override //va correr en consola.
+	public void run(String... args) throws Exception {
+		List<Person> persons = (List<Person>) personRepository.findAll();
+		persons.stream().forEach(person -> System.out.println(person));
+	}
 }
