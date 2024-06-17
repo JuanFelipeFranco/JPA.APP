bdae8b3dde87f0af514074f334b7a0f07b8b1819
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-17 18:42:53 -0500

    Update Usuario

--- JPA.APP/src/main/java/com/Felipe/JPA/APP/Application.java
+++ JPA.APP/src/main/java/com/Felipe/JPA/APP/Application.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -24,12 +26,57 @@ public class Application implements CommandLineRunner {
 	@Override //va correr en consola.
 	public void run(String... args) throws Exception {
 //		findOne();
-		create();
+//		create();
+		update();
 	}
+	@Transactional
+	public void update(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese id de la persona: ");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = personRepository.findById(id);
+
+		/*optionalPerson.ifPresent(person -> {
+			System.out.println(person);
+			System.out.println("Ingrese el lenguaje de programacion: ");
+			String programmingLanguage = scanner.next();
+			person.setProgramingLanguage(programmingLanguage);
+			Person personDb = personRepository.save(person);
+			System.out.println(personDb);
+		});*/
+
+		if(optionalPerson.isPresent()){
+			Person person = optionalPerson.orElseThrow();
+			System.out.println(person);
+			System.out.println("Ingrese el lenguaje de programacion: ");
+			String programmingLanguage = scanner.next();
+			person.setProgramingLanguage(programmingLanguage);
+			Person personDb = personRepository.save(person);
+			System.out.println(personDb);
+		}else {
+			System.out.println("Usuario no esta presente no existe");
+		}
+
+		scanner.close();
+	}
+	@Transactional
 	public void create(){
-		Person person = new Person(null,"Juan","Franco","Java");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese nombre: ");
+		String name = scanner.next();
+		System.out.println("Ingrese apellido: ");
+		String lastname = scanner.next();
+		System.out.println("Ingrese Lenguaje de programacion: ");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+
+		Person person = new Person(null,name,lastname,programmingLanguage);
+
 		Person personNew = personRepository.save(person);
 		System.out.println(personNew);
+
+		personRepository.findById(personNew.getId()).ifPresent(System.out::println);
 	}
 	public void findOne(){
 		//primer manera
